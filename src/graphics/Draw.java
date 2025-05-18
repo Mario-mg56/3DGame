dde05fa2d15b0d0ba8488925f5b02d9b07e062e1
Mario <mario.m.g.5557@gmail.com>
2025-05-19 01:09:36 +0200

    update Draw

--- src/graphics/Draw.java
+++ src/graphics/Draw.java
@@ -8,20 +8,20 @@ import static org.lwjgl.opengl.GL11.*;
 
 public class Draw {
     public static void fill(Color color) {
-        GL11.glClearColor(color.r, color.g, color.b, 1.0f);
+        GL11.glClearColor(color.r/255, color.g/255, color.b/255, 1.0f);
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
     }
 
     public static void drawPoint(Point2 point, Color color) {
         glPointSize(5);
-        glColor3f(color.r, color.g, color.b);
+        glColor3f(color.r/255, color.g/255, color.b/255);
         glBegin(GL_POINTS);
             glVertex2f(point.x, point.y);
         glEnd();
     }
     public static void drawPoint(Point2 point, Float[] rgb) {
         glPointSize(5);
-        glColor3f(rgb[0], rgb[1], rgb[2]);
+        glColor3f(rgb[0]/255, rgb[1]/255, rgb[2]/255);
         glBegin(GL_POINTS);
             glVertex2f(point.x, point.y);
         glEnd();
@@ -29,7 +29,7 @@ public class Draw {
 
     public static void drawLine(Point2 start, Point2 end, Color color) {
         glLineWidth(5);
-        glColor3f(color.r, color.g, color.b);
+        glColor3f(color.r/255, color.g/255, color.b/255);
         glBegin(GL_LINES);
             glVertex2f(start.x, start.y);
             glVertex2f(end.x, end.y);
@@ -37,21 +37,22 @@ public class Draw {
     }
     public static void drawLine(Point2 start, Point2 end, Float[] rgb) {
         glLineWidth(5);
-        glColor3f(rgb[0], rgb[1], rgb[2]);
+        glColor3f(rgb[0]/255, rgb[1]/255, rgb[2]/255);
         glBegin(GL_LINES);
             glVertex2f(start.x, start.y);
             glVertex2f(end.x, end.y);
         glEnd();
     }
 
+    //Importante puntos en sentido antihorario empezando por arriba a la izq
     public static void drawPoly(ArrayList<Point2> points, Color color) {
-        glColor3f(color.r, color.g, color.b);
+        glColor3f(color.r/255, color.g/255, color.b/255);
         glBegin(GL_POLYGON);
             for (Point2 p : points) {glVertex2f(p.x, p.y);}
         glEnd();
     }
     public static void drawPoly(ArrayList<Point2> points, Float[] rgb) {
-        glColor3f(rgb[0], rgb[1], rgb[2]);
+        glColor3f(rgb[0]/255, rgb[1]/255, rgb[2]/255);
         glBegin(GL_POLYGON);
             for (Point2 p : points) {glVertex2f(p.x, p.y);}
         glEnd();
